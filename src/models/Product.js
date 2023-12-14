707f218dca2dbbf0023f006e5f74e2efdc424b5e
Kori Ditmeyer <kori.ditmeyer@gmail.com>
2023-12-14 17:33:37 +0900

    Add multiple pictures options with multer for the products endpoints

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,21 +1,31 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
 const productSchema = new Schema(
   {
-    _id: { type: String, required: true }, 
+    _id: { type: String, required: true },
     title: { type: String, required: true },
+    attribute: { type: String },
     description: { type: String, required: true },
-    code: { type: String, required: true,unique: true },
+    brand: { type: String },
+    code: { type: String, required: true, unique: true },
+    avgRating: { type: Number },
+    ratings: { type: Number },
     price: { type: Number, required: true },
-    status: { type: String, default:true, required: false }, // Set status to true by default
+    oldPrice: { type: Number },
+    status: { type: String, default: true, required: false }, // Set status to true by default
     stock: { type: Number, required: true },
     category: { type: String, required: true },
-    thumbnail: { type: Array, default:["static\\img\\products\\defaultProduct.jpeg"],required: false }, // Set thumbnail as an array of strings, default to an empty array
+    badge: { type: String },
+    thumbnail: {
+      type: Array,
+      default: ["static\\img\\products\\defaultProduct.jpeg"],
+      required: false,
+    }, // Set thumbnail as an array of strings, default to an empty array
   },
   {
     versionKey: false,
-    // ACTIVATE THAT MONGOBD RETURN ERROR 
-    strict: 'throw',
+    // ACTIVATE THAT MONGOBD RETURN ERROR
+    strict: "throw",
     // CAN ADD METHODS AND STATIC METHODS BELOW
     methods: {
       hi() {
@@ -23,10 +33,10 @@ const productSchema = new Schema(
       },
     },
     statics: {
-      count(){
-        return 1
-      }
-    }
+      count() {
+        return 1;
+      },
+    },
   }
 );
 
